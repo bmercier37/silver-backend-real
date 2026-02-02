3ee8f5f1e4e67a2d9b04583470b08cc62f45db90
bmercier37 <bmercier37@yahoo.fr>
2026-02-02 18:05:02 +0100

    Update fetchData.js

--- fetchData.js
+++ fetchData.js
@@ -1,5 +1,5 @@
 // fetchData.js
-import { scrapeSilverNY, scrapeSilverLondon, scrapeSilverShanghai, scrapeGoldNY, scrapeFXRateRMBUSD } from "./scrapeChinaFX.js";
+import { scrapeSilverNY, scrapeSilverLondon, scrapeSilverShanghai, scrapeSilverDE, scrapeGoldNY, scrapeFXRateUSDRMB, scrapeFXRateUSDEUR } from "./scrapeChinaFX.js";
 import { initDB, insertData } from "./db.js";
 
 export async function fetchAndStore() {
@@ -15,20 +15,30 @@ if (silverLondon == null) console.warn("⚠️ Scraping failed: Silver London");
 const silverSHA_RMB = await scrapeSilverShanghai();
 if (silverSHA_RMB == null) console.warn("⚠️ Scraping failed: Silver Shanghai (RMB)");
 
+const silverDE_EUR = await scrapeSilverDE();
+if (silverSHA_RMB == null) console.warn("⚠️ Scraping failed: Silver Shanghai (RMB)");
+
 const goldNY = await scrapeGoldNY();
 if (goldNY == null) console.warn("⚠️ Scraping failed: Gold NY");
 
-const FX_RMB_USD = await scrapeFXRateRMBUSD();
+const FX_USD_RMB = await scrapeFXRateUSDRMB();
 if (FX_RMB_USD == null) console.warn("⚠️ Scraping failed: FX USD/CNY");
 
+const FX_USD_EUR = await scrapeFXRateUSDEUR();
+if (FX_RMB_USD == null) console.warn("⚠️ Scraping failed: FX USD/EUR");
 
 
-  // Conversion Silver SHA kg → oz
-  const silverSHA = silverSHA_RMB * FX_RMB_USD / 31.1035;
+  // Conversion Silver SHA kg → oz & RMB →USD 
+  const silverSHA = silverSHA_RMB * FX_USD_RMB / 31.1035;
+
+  // Conversion Silver DE EUR →USD 
+  const silverDE = silverDE_EUR * FX_USD_EUR;
 
   // Calcul ratios et spreads
   const goldSilverRatio = goldNY / silverNY;
   const spreadSHA_NY = ((silverSHA - silverNY) / silverNY) * 100;
+  const spreadDE_NY = ((silverDE - silverNY) / silverNY) * 100;
+
 
   // Préparer l'objet de données correspondant aux colonnes SQLite
   const data = {
@@ -36,10 +46,13 @@ if (FX_RMB_USD == null) console.warn("⚠️ Scraping failed: FX USD/CNY");
     silverNY,
     silverLondon,
     silverSHA,
+    silverDE,
     goldNY,
-    FX_RMB_USD,
+    FX_USD_RMB,
+    FX_USD_EUR,
     goldSilverRatio,
-    spreadSHA_NY
+    spreadSHA_NY,
+    spreadDE_NY
   };
 
   await insertData(db, data);
